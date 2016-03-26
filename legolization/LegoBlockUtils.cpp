eaa54ef79bd90ac215dc3587a3d8626ec041cd57
bijoy <bijoysingh693@gmail.com>
2016-03-26 20:30:11 +0530

    Changing class name

--- legolization/LegoBlockUtils.cpp
+++ legolization/LegoBlockUtils.cpp
@@ -2,27 +2,27 @@
 // Created by Bijoy Kochar on 3/26/16.
 //
 
-#include "LegoBlockSet.h"
+#include "LegoBlockUtils.h"
 #include "LegoBlock.h"
 
-bool LegoBlockSet::can_merge(LegoBlock *b1, LegoBlock *b2) {
+bool LegoBlockUtils::can_merge(LegoBlock *b1, LegoBlock *b2) {
     return (can_colors_merge(b1, b2)
             && b1->z == b2->z
             && (b1->x == b2->x && b1->sx == b2->sx && LegoBlock::has_valid_size(b1->x, b1->y + b2->y))
             && (b1->y == b2->y && b1->sy == b2->sy && LegoBlock::has_valid_size(b1->x + b2->x, b1->y)));
 }
 
-bool LegoBlockSet::can_colors_merge(LegoBlock *b1, LegoBlock *b2) {
+bool LegoBlockUtils::can_colors_merge(LegoBlock *b1, LegoBlock *b2) {
     return (b1->ignore_color
             || b2->ignore_color
             || (b1->r == b2->r && b1->g == b2->g && b1->b == b2->b));
 }
 
-bool LegoBlockSet::are_connected(LegoBlock *b1, LegoBlock *b2) {
+bool LegoBlockUtils::are_connected(LegoBlock *b1, LegoBlock *b2) {
     return is_stacked_on(b1, b2) || is_stacked_on(b2, b1);
 }
 
-bool LegoBlockSet::is_stacked_on(LegoBlock *b1, LegoBlock *b2) {
+bool LegoBlockUtils::is_stacked_on(LegoBlock *b1, LegoBlock *b2) {
     bool stacked_z = b2->z == b1->z + 1;
     bool overlapping_x = b1->x == b2->x
                          || (b1->x < b2->x && b1->x + b1->sx > b2->x)
@@ -34,7 +34,7 @@ bool LegoBlockSet::is_stacked_on(LegoBlock *b1, LegoBlock *b2) {
     return stacked_z && overlapping_x && overlapping_y;
 }
 
-int LegoBlockSet::get_merge_color(LegoBlock *b1, LegoBlock *b2) {
+int LegoBlockUtils::get_merge_color(LegoBlock *b1, LegoBlock *b2) {
     assert(can_colors_merge(b1, b2));
 
     if (b1->ignore_color && b2->ignore_color)
@@ -44,7 +44,7 @@ int LegoBlockSet::get_merge_color(LegoBlock *b1, LegoBlock *b2) {
     return 1;
 }
 
-LegoBlock *LegoBlockSet::merge(LegoBlock *b1, LegoBlock *b2) {
+LegoBlock *LegoBlockUtils::merge(LegoBlock *b1, LegoBlock *b2) {
     assert(can_merge(b1, b2));
 
     LegoBlock *b;
--- legolization/LegoBlockUtils.h
+++ legolization/LegoBlockUtils.h
@@ -7,26 +7,26 @@
 
 #include <algorithm>
 
-class LegoBlockSet {
+class LegoBlockUtils {
 public:
 
     // Merge Blocks
-    LegoBlock merge(LegoBlock *block_1, LegoBlock *block_2);
+    static LegoBlock merge(LegoBlock *block_1, LegoBlock *block_2);
 
     // Checks if 2 Lego blocks are connected
-    bool are_connected(LegoBlock *block_1, LegoBlock *block_2);
+    static bool are_connected(LegoBlock *block_1, LegoBlock *block_2);
 
     // Checks if block 2 is stacked over block 1
-    bool is_stacked_on(LegoBlock *block_1, LegoBlock *block_2);
+    static bool is_stacked_on(LegoBlock *block_1, LegoBlock *block_2);
 
     // Checks if 2 Lego Blocks can merge (checks if colors are compatible)
-    bool can_merge(LegoBlock *block_1, LegoBlock *block_2);
+    static bool can_merge(LegoBlock *block_1, LegoBlock *block_2);
 
     // Checks if 2 Lego Blocks can merge wrt color
-    bool can_colors_merge(LegoBlock *block_1, LegoBlock *block_2);
+    static bool can_colors_merge(LegoBlock *block_1, LegoBlock *block_2);
 
     // Returns which block's color should be considered. 0 if ignore
-    int get_merge_color(LegoBlock *block_1, LegoBlock *block_2);
+    static int get_merge_color(LegoBlock *block_1, LegoBlock *block_2);
 
 };
 
